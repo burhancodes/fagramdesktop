2663f517bf9c5a84fed216d1d92192ca73bfcf61
FajoX <146753912+FajoX1@users.noreply.github.com>
2024-09-15 18:43:59 +0300

    org.fagram
    i think it will compile. please.

--- Telegram/SourceFiles/platform/linux/specific_linux.cpp
+++ Telegram/SourceFiles/platform/linux/specific_linux.cpp
@@ -236,7 +236,7 @@ bool GenerateDesktopFile(
 	DEBUG_LOG(("App Info: placing .desktop file to %1").arg(targetPath));
 	if (!QDir(targetPath).exists()) QDir().mkpath(targetPath);
 
-	const auto sourceFile = u":/misc/org.telegram.desktop.desktop"_q;
+	const auto sourceFile = u":/misc/org.fagram.desktop.desktop"_q;
 	const auto targetFile = targetPath
 		+ QGuiApplication::desktopFileName()
 		+ u".desktop"_q;
@@ -375,7 +375,7 @@ bool GenerateDesktopFile(
 		hashMd5Hex(d.constData(), d.size(), md5Hash);
 
 		if (!Core::Launcher::Instance().customWorkingDir()) {
-			QFile::remove(u"%1org.telegram.desktop._%2.desktop"_q.arg(
+			QFile::remove(u"%1org.fagram.desktop._%2.desktop"_q.arg(
 				targetPath,
 				md5Hash));
 
@@ -384,7 +384,7 @@ bool GenerateDesktopFile(
 			hashMd5Hex(exePath.constData(), exePath.size(), md5Hash);
 		}
 
-		QFile::remove(u"%1org.telegram.desktop.%2.desktop"_q.arg(
+		QFile::remove(u"%1org.fagram.desktop.%2.desktop"_q.arg(
 			targetPath,
 			md5Hash));
 	}
@@ -443,7 +443,7 @@ bool GenerateServiceFile(bool silent = false) {
 		const auto d = QFile::encodeName(QDir(cWorkingDir()).absolutePath());
 		hashMd5Hex(d.constData(), d.size(), md5Hash);
 
-		QFile::remove(u"%1org.telegram.desktop._%2.service"_q.arg(
+		QFile::remove(u"%1org.fagram.desktop._%2.service"_q.arg(
 			targetPath,
 			md5Hash));
 	}
@@ -693,10 +693,10 @@ void start() {
 					md5Hash.data());
 			}
 
-			return u"org.telegram.desktop._%1"_q.arg(md5Hash.constData());
+			return u"org.fagram.desktop._%1"_q.arg(md5Hash.constData());
 		}
 
-		return u"org.telegram.desktop"_q;
+		return u"org.fagram.desktop"_q;
 	}());
 
 	LOG(("App ID: %1").arg(QGuiApplication::desktopFileName()));
