7aa09ec1241c654ab4822409891d0dff7c63f848
FajoX <146753912+FajoX1@users.noreply.github.com>
2024-09-23 15:59:03 +0300

    change version

---
+++ Telegram/SourceFiles/fa/fa_version.h
@@ -0,0 +1,13 @@
+/*
+This file is part of FAgram Desktop,
+the unofficial desktop client based on Telegram Desktop.
+
+For license and copyright information please follow this link:
+https://github.com/fajox1/fagramdesktop/blob/master/LEGAL
+*/
+
+#pragma once
+
+constexpr auto AppFAVersion = 1000001;
+constexpr auto AppFAVersionStr = "1.0.1";
+constexpr auto AppFABetaVersion = true;
