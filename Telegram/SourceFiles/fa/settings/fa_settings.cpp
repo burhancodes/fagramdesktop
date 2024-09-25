5e1b7c4e5e1b30263f3b9674c58f06318e6eb441
FajoX <146753912+FajoX1@users.noreply.github.com>
2024-09-25 09:46:07 +0300

    fucking comma

--- Telegram/SourceFiles/fa/settings/fa_settings.cpp
+++ Telegram/SourceFiles/fa/settings/fa_settings.cpp
@@ -188,7 +188,7 @@ const std::map<QString, Definition, std::greater<QString>> DefinitionMap {
 		.defaultValue = true, }},
 	{ "disable_ads", {
 		.type = SettingType::BoolSetting,
-		.defaultValue = false, }}
+		.defaultValue = false, }},
 	{ "recent_stickers_limit", {
 		.type = SettingType::IntSetting,
 		.defaultValue = 30,
