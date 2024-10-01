dfcf545da273528291a1c9161b6a36ac05d130d9
FajoX <146753912+FajoX1@users.noreply.github.com>
2024-10-01 19:56:53 +0300

    Update fa_appearance.h

--- Telegram/SourceFiles/fa/settings_menu/sections/fa_appearance.h
+++ Telegram/SourceFiles/fa/settings_menu/sections/fa_appearance.h
@@ -10,12 +10,13 @@ https://github.com/fajox1/fagramdesktop/blob/master/LEGAL
 
 #include "settings/settings_common.h"
 #include "settings/settings_common_session.h"
+#include <QLabel>
+#include <QSlider>
 
 class BoxContent;
 
 namespace Window {
     class Controller;
-
     class SessionController;
 } // namespace Window
 
@@ -28,9 +29,12 @@ namespace Settings {
 
     private:
         void SetupAppearance(not_null<Ui::VerticalLayout *> container);
-        
-        void SetupFAAppearance(not_null<Ui::VerticalLayout *> container, not_null<Window::SessionController *> null);
+        void SetupFAAppearance(not_null<Ui::VerticalLayout *> container, not_null<Window::SessionController *> controller);
         void setupContent(not_null<Window::SessionController *> controller);
+
+        QLabel *userpicRoundnessLabel;
+        QLabel *roundnessPreview;
+        QSlider *userpicRoundnessSlider;
     };
 
 } // namespace Settings
