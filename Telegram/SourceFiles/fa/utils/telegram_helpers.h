936a15ac60bc0fd6f3873ad06cd9151a8bfdf556
FajoX <146753912+FajoX1@users.noreply.github.com>
2024-10-03 21:40:45 +0300

    fix

--- Telegram/SourceFiles/fa/utils/telegram_helpers.h
+++ Telegram/SourceFiles/fa/utils/telegram_helpers.h
@@ -8,8 +8,12 @@ https://github.com/fajox1/fagramdesktop/blob/master/LEGAL
 
 #pragma once
 
+#include "apiwrap.h"
+#include "api/api_text_entities.h"
+
+#include "lang_auto.h"
 #include "fa/data/entities.h"
-#include "data/data_peer.h"
+#include "core/mime_type.h"
 #include "data/data_channel.h"
 #include "data/data_forum.h"
 #include "data/data_forum_topic.h"
@@ -17,11 +21,19 @@ https://github.com/fajox1/fagramdesktop/blob/master/LEGAL
 #include "data/data_peer_id.h"
 #include "data/data_photo.h"
 #include "data/data_user.h"
+#include "data/data_peer.h"
+#include "inline_bots/inline_bot_result.h"
+
 #include "core/application.h"
+#include "data/data_document.h"
+#include "data/data_session.h"
+#include "history/history.h"
+#include "history/history_item.h"
+#include "history/history_item_components.h"
+#include "history/history_unread_things.h"
 #include "main/main_account.h"
-#include "main/main_domain.h"
 #include "main/main_session.h"
-#include "dialogs/dialogs_main_list.h"
+#include "ui/text/format_values.h"
 
 #include <string>     
 #include <utility>    
