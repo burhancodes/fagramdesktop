c9c26387107aaf51e4985b3c918241d0215602e9
FajoX <146753912+FajoX1@users.noreply.github.com>
2025-01-23 16:54:27 +0200

    move: 1.1.5 -> 1.1.6

--- Telegram/SourceFiles/fa/fa_version.h
+++ Telegram/SourceFiles/fa/fa_version.h
@@ -8,7 +8,7 @@ https://github.com/fajox1/fagramdesktop/blob/master/LEGAL
 
 #pragma once
 
-constexpr auto AppFAVersion = 1000015;
-constexpr auto AppFAVersionStr = "1.1.5";
-constexpr auto AppTGDVersion = "5.10.0";
+constexpr auto AppFAVersion = 1000016;
+constexpr auto AppFAVersionStr = "1.1.6";
+constexpr auto AppTGDVersion = "5.10.4";
 constexpr auto AppFABetaVersion = false;
